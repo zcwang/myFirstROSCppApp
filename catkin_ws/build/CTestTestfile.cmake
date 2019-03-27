157250f84e731a6feeeea558f14ecc35c2435593
Gary Wang <garywang@itri.org.tw>
2019-03-27 13:33:40 +0800

    My first ROS cpp-based App

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/garywang/work/test/catkin_ws/src
+# Build directory: /home/garywang/work/test/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(beginner_tutorials)
